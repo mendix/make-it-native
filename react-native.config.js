562b69c1d36e8af608d9b3b4d70e0c2c2c95f324
Uraz Akgültan <uraz@akgultan.com>
2024-05-01 08:44:17 +0200

    Apply changes for 10.9.0

--- react-native.config.js
+++ react-native.config.js
@@ -1,15 +1,9 @@
 module.exports = {
   dependencies: {
-    'react-native-code-push': {
-      platforms: {
-        android: null, // disable Android platform, other platforms will still autolink if provided
-        ios: null, // disable ios platform, CodePush is not necessary for the MendixNative library
-      },
-    },
-    'react-native-video': {
+    "react-native-video": {
       platforms: {
         android: {
-          sourceDir: '../node_modules/react-native-video/android-exoplayer',
+          sourceDir: "../node_modules/react-native-video/android-exoplayer",
         },
       },
     },
