a01779690f667328fef2a8166f71b820aa82aa63
Pavel Hryn <pavel.hryn@mendix.com>
2024-06-06 09:49:03 +0200

    Upgrade Android part to RN 0.73.8

--- react-native.config.js
+++ react-native.config.js
@@ -1,11 +1,5 @@
 module.exports = {
   dependencies: {
-    'react-native-video': {
-      platforms: {
-        android: {
-          sourceDir: '../node_modules/react-native-video/android-exoplayer',
-        },
-      },
-    },
+    // Define your dependencies config there
   },
 };
