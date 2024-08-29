39a82c4d92c088d273ac8dd51379877ab97affcb
karahanharunn <karahan.harunn@gmail.com>
2024-08-29 17:15:44 +0200

    fix: react-native-video version update

--- react-native.config.js
+++ react-native.config.js
@@ -1,11 +1,3 @@
 module.exports = {
-  dependencies: {
-    'react-native-video': {
-      platforms: {
-        android: {
-          sourceDir: '../node_modules/react-native-video/android-exoplayer',
-        },
-      },
-    },
-  },
+  dependencies: {},
 };
