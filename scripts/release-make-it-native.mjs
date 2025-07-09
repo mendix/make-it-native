c7e2a5c9809d4f0bfd37b4f0d27f597ce228ed07
Uraz Akgültan <uraz@akgultan.com>
2025-07-09 07:38:09 +0200

    fix: fix changelog automation pipeline

--- scripts/release-make-it-native.mjs
+++ scripts/release-make-it-native.mjs
@@ -70,18 +70,8 @@ function updateChangelog({ changelog, unreleasedContent, changelogPath }) {
 async function createPRUpdateChangelog() {
   const git = simpleGit();
 
-  await git.addConfig(
-    "user.name",
-    GIT_AUTHOR_NAME,
-    false,
-    GitConfigScope.global
-  );
-  await git.addConfig(
-    "user.email",
-    GIT_AUTHOR_EMAIL,
-    false,
-    GitConfigScope.global
-  );
+  await git.addConfig("user.name", GIT_AUTHOR_NAME);
+  await git.addConfig("user.email", GIT_AUTHOR_EMAIL);
 
   await git.checkoutLocalBranch(CHANGELOG_BRANCH_NAME);
 
