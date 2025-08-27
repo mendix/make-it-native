bc80c73e5d48f7743be8ddfc40804b8a7e055afc
karahanharunn <karahann.harun@gmail.com>
2025-08-27 15:37:20 +0200

    removed left overs about splash screen

--- android/app/src/production/java/com/mendix/developerapp/splashscreen/SplashScreenPresenter.kt
+++ android/app/src/production/java/com/mendix/developerapp/splashscreen/SplashScreenPresenter.kt
@@ -1,16 +1,12 @@
-package com.mendix.developerapp.splashscreen
-
 import android.app.Activity
-import com.mendix.mendixnative.react.splash.MendixSplashScreenPresenter
+import com.zoontek.rnbootsplash.RNBootSplash
 
-class SplashScreenPresenter : MendixSplashScreenPresenter {
+class SplashScreenPresenter {
     override fun show(activity: Activity) {
-        // react-native-bootsplash handles the native splash screen automatically
-        // No need to manually show it here
+        RNBootSplash.init(activity)
     }
 
     override fun hide(activity: Activity) {
-        // react-native-bootsplash will be hidden from JavaScript using RNBootSplash.hide()
-        // No need to manually hide it here
+        RNBootSplash.hide(activity, true)
     }
 }
