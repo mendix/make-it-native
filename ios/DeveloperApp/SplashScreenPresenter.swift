f326a273acd1cf80746965105932dd3fe5a64dbf
Selim Ustel <stelselim.devs@gmail.com>
2025-09-10 11:26:55 +0200

    add splash functions

--- ios/DeveloperApp/SplashScreenPresenter.swift
+++ ios/DeveloperApp/SplashScreenPresenter.swift
@@ -3,12 +3,13 @@ import Foundation
 class SplashScreenPresenter: SplashScreenPresenterProtocol  {
   func show(_ rootView: UIView?) {
     if (rootView != nil) {
-      RNBootSplash.initWithStoryboard("LaunchScreen", rootView: rootView!)
+      RNBootSplash.showStoryBoard("LaunchScreen", rootView: rootView!)
     }
   }
 
   func hide() {
     // RNBootSplash automatically handles hiding when the React Native app is ready
     // No explicit hide call needed with the new API
+    RNBootSplash.hideStoryBoard()
   }
 }
