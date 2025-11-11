3b8ccaeba347d37d2f72f3b4c98059ecacf21547
MxKevinBeqo <kevin.beqo@mendix.com>
2025-11-11 17:03:08 +0100

    fix: input focus propagation on android

--- android/app/src/production/java/com/mendix/developerapp/mendixapp/MendixProjectFragmentBase.kt
+++ android/app/src/production/java/com/mendix/developerapp/mendixapp/MendixProjectFragmentBase.kt
@@ -5,35 +5,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
-import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.viewModels
 import com.mendix.developerapp.MendixBaseFragment
+import com.mendix.developerapp.R
 import com.mendix.developerapp.loading.ProjectLoaderViewModel
-import com.mendix.developerapp.ui.theme.MyApplicationTheme
 import com.mendix.developerapp.utilities.GlobalTouchEventListener
 import com.zoontek.rnbootsplash.RNBootSplash
 import com.mendix.developerapp.R
 
 open class MendixProjectFragmentBase : MendixBaseFragment(), GlobalTouchEventListener {
-    private lateinit var composeView: ComposeView
     protected val viewModel: ProjectLoaderViewModel by viewModels()
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        composeView.setContent {
-            MyApplicationTheme {
-                MendixProjectScreen(viewModel)
-            }
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = super.onCreateView(inflater, container, savedInstanceState)
+        val rootView = inflater.inflate(R.layout.fragment_mendix_project_traditional, container, false)
+        val reactNativeContainer = rootView.findViewById<FrameLayout>(R.id.react_native_container)
+        val reactRootView = super.onCreateView(inflater, container, savedInstanceState)
 
         viewModel.status.observe(viewLifecycleOwner) {
             if (it === ProjectLoaderViewModel.STATUS_SUCCESS) {
@@ -43,22 +33,13 @@ open class MendixProjectFragmentBase : MendixBaseFragment(), GlobalTouchEventLis
             }
         }
 
-        /**
-         * Why aren't we just attaching to ReactRootView directly? ReactRootView is fairly special.
-         * Layouting is actually handled by the UIManagerModule, which means whatever child is added out of React Native's context
-         * to the ReactRootView is basically ignored and gets no layout call nor parameters.
-         */
-        val frameLayout = FrameLayout(requireContext())
-        frameLayout.layoutParams = FrameLayout.LayoutParams(
-            ViewGroup.LayoutParams.MATCH_PARENT,
-            ViewGroup.LayoutParams.MATCH_PARENT
+        reactNativeContainer.addView(reactRootView,
+            FrameLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT
+            )
         )
-        frameLayout.addView(view)
 
-        viewModel.setRNFrameLayout(frameLayout)
-
-        return ComposeView(requireContext()).also {
-            composeView = it
-        }
+        return rootView
     }
 }
