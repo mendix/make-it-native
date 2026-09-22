1711536f479586c6a864302303dc2854bb48f043
Yogendra Shelke <yogendrashelke1992@gmail.com>
2026-09-22 17:28:14 +0530

    fix: use nativeBinaryVersion from mendix-native

--- ios/DeveloperApp/Home/HomeViewController.swift
+++ ios/DeveloperApp/Home/HomeViewController.swift
@@ -169,10 +169,12 @@ class HomeViewController: UIViewController {
                     uiState = .deprecatedRuntime
                     return cb(false)
                 }
-                guard let runtimeInfo = response.runtimeInfo, let supportedNativeBinaryVersion = MxConfigProxy.prepare()?.nativeBinaryVersion.intValue else {
+                guard let runtimeInfo = response.runtimeInfo else {
                     uiState = .deprecatedRuntime
                     return cb(false)
                 }
+                
+                let supportedNativeBinaryVersion = MxConfiguration.nativeBinaryVersion
 
                 if (runtimeInfo.nativeBinaryVersion == supportedNativeBinaryVersion) {
                     return cb(true)
